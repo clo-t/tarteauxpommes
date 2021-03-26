889d80701ecdebbe90b8c991c8df210a6b2d7309
ichbinrob <robinson.lacotte@edu.gobelins.fr>
2021-03-26 19:27:42 +0100

    test role

--- bot.js
+++ bot.js
@@ -3,16 +3,18 @@ const LIST = require ("./list.js")
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
+//const guild = new Discord.Guild()
 
 client.on("ready", () => {
   console.log("I am ready!");
+  console.log(client.guilds.roles)
+  // avalanche id 619131910391857153
 });
 
 client.on("message", (message) => {
-    console.log(message)
+    // console.log(message)
 if (message.author.bot !== true) {
   for (const key in LIST) {
-    console.log(key)
     if (Object.hasOwnProperty.call(LIST, key) && message.content.toLowerCase().includes(key)) {
         message.channel.send(LIST[key]);
     }
