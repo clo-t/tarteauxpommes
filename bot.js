da055d303208acd76f96e07d9b9afe18cea152a2
IchbinRob <robinson.lacotte@edu.gobelins.fr>
2021-03-27 11:15:09 +0100

    prettier

--- bot.js
+++ bot.js
@@ -1,26 +1,24 @@
-const TOKEN = require("./token.js");
-const LIST = require ("./list.js")
+const TOKEN = require("./token.js")
+const LIST = require("./list.js")
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require("discord.js")
+const client = new Discord.Client()
 //const guild = new Discord.Guild()
 
 client.on("ready", () => {
-  console.log("I am ready!");
-  console.log(client.guilds.roles)
+  console.log("I am ready!")
   // avalanche id 619131910391857153
-});
+})
 
 client.on("message", (message) => {
-    // console.log(message)
-if (message.author.bot !== true) {
-  for (const key in LIST) {
-    if (Object.hasOwnProperty.call(LIST, key) && message.content.toLowerCase().includes(key)) {
-        message.channel.send(LIST[key]);
+  // console.log(message)
+  if (message.author.bot !== true) {
+    for (const key in LIST) {
+      if (Object.hasOwnProperty.call(LIST, key) && message.content.toLowerCase().includes(key)) {
+        message.channel.send(LIST[key])
+      }
     }
   }
+})
 
-}
-});
-
-client.login(TOKEN);
+client.login(TOKEN)
