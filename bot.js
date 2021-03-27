6de65060d750ce83602f31b5ad61f7b86fc18485
IchbinRob <robinson.lacotte@edu.gobelins.fr>
2021-03-27 18:41:27 +0100

    remove command

--- bot.js
+++ bot.js
@@ -11,13 +11,13 @@ const fs = require("fs")
 client.commands = new Discord.Collection()
 client.listenersScript = new Discord.Collection()
 
-const commandFiles = fs.readdirSync("./commands/").filter((file) => file.endsWith(".js"))
+// const commandFiles = fs.readdirSync("./commands/").filter((file) => file.endsWith(".js"))
 const listenersFiles = fs.readdirSync("./listeners/").filter((file) => file.endsWith(".js"))
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
+// for (const file of commandFiles) {
+//   const command = require(`./commands/${file}`)
 
-  client.commands.set(command.name, command)
-}
+//   client.commands.set(command.name, command)
+// }
 
 for (const file of listenersFiles) {
   const listener = require(`./listeners/${file}`)
