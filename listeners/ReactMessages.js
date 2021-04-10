4ca416434b34591e3cee988b14e303646add6a3e
ichbinrob <robinson.lacotte@edu.gobelins.fr>
2021-04-10 15:46:47 +0200

    don't listen critical channel

--- listeners/ReactMessages.js
+++ listeners/ReactMessages.js
@@ -2,6 +2,9 @@ module.exports = {
   name: "reactmessages",
   description: "react to messages",
   async listenMessages(LIST, message) {
+    if (message.channel.id === "639480012600377344" || message.channel.id === "693171926432088215")
+      return
+
     for (const key in LIST) {
       if (Object.hasOwnProperty.call(LIST, key) && message.content.toLowerCase().includes(key)) {
         message.channel.send(LIST[key])
