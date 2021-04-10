224f25a506b0e632efd5883fa304c74863555fc1
ichbinrob <robinson.lacotte@edu.gobelins.fr>
2021-04-10 15:51:21 +0200

    add blacklisted channel

--- listeners/ReactMessages.js
+++ listeners/ReactMessages.js
@@ -2,7 +2,12 @@ module.exports = {
   name: "reactmessages",
   description: "react to messages",
   async listenMessages(LIST, message) {
-    if (message.channel.id === "639480012600377344" || message.channel.id === "693171926432088215")
+    if (
+      message.channel.id === "639480012600377344" ||
+      message.channel.id === "693171926432088215" ||
+      message.channel.id === "625977705480847390" ||
+      message.channel.id === "636530884287266816"
+    )
       return
 
     for (const key in LIST) {
