7a2c33e674f8f0d07ac02c04c092044c9c167822
Clo T <clo.tay@gmail.com>
2021-11-11 17:26:52 +0100

    play a single music from youtube

--- commands/play.js
+++ commands/play.js
@@ -1,64 +1,70 @@
-const ytdl = require('ytdl-core');
-const ytSearch = require ('yt-search');
+const ytdl = require("ytdl-core");
+const ytSearch = require("yt-search");
 
 module.exports = {
-    name: 'play',
-    description: 'play music from YT in voice channel',
-    async execute(client, message, args){
-        const voiceChannel = message.member.voice.channel;
- 
-        if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
+  name: "play",
+  description: "play music from YT in voice channel",
+  async execute(client, message, args) {
+    const voiceChannel = message.member.voice.channel;
 
-        const permissions = voiceChannel.permissionsFor(message.client.user);
-        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
-        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+    if (!voiceChannel)
+      return message.channel.send(
+        "You need to be in a channel to execute this command!"
+      );
 
-        if (!args.length) return message.channel.send('You need to send the second argument!');
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK"))
+      return message.channel.send("You dont have the correct permissions");
 
-        const validURL = (str) =>{
-            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-            return regex.test(str);
-        }
- 
-        if(validURL(args[0])){
- 
-            const  connection = await voiceChannel.join();
-            const stream  = ytdl(args[0], {filter: 'audioonly'});
- 
-            connection.play(stream, {seek: 0, volume: 1})
-            .on('finish', () =>{
-                voiceChannel.leave();
-                message.channel.send('leaving channel');
-            });
- 
-            await message.reply(`:music: Now Playing ***Your Link!***`)
- 
-            return
-        }
- 
-        
-        const  connection = await voiceChannel.join();
- 
-        const videoFinder = async (query) => {
-            const videoResult = await ytSearch(query);
- 
-            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
- 
-        }
- 
-        const video = await videoFinder(args.join(' '));
- 
-        if(video){
-            const stream  = ytdl(video.url, {filter: 'audioonly'});
-            connection.play(stream, {seek: 0, volume: 1})
-            .on('finish', () =>{
-                voiceChannel.leave();
-            });
- 
-            await message.reply(`:music: Now Playing ***${video.title}***`)
-        } else {
-            message.channel.send('No video results found');
-        }
-    
+    if (!args.length)
+      return message.channel.send("You need to send the second argument!");
+
+    const validURL = (str) => {
+      var regex =
+        /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+      return regex.test(str);
+    };
+
+    const connection = await voiceChannel.join();
+
+    if (validURL(args[0])) {
+      if (!ytdl.validateURL(args[0])) {
+        message.channel.send("only youtube url for now");
+        return;
+      }
+
+      const stream = ytdl(args[0], { filter: "audioonly" });
+
+      connection.play(stream, { seek: 0, volume: 1 }).on("finish", () => {
+        voiceChannel.leave();
+      });
+
+      await message.reply(
+        `:notes: Now Playing *** ${await (
+          await ytdl.getBasicInfo(args[0])
+        ).player_response.videoDetails.title}***`
+      );
+
+      return;
     }
-}
+
+    const videoFinder = async (query) => {
+      const videoResult = await ytSearch(query);
+
+      return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
+    };
+
+    const video = await videoFinder(args.join(" "));
+
+    if (video) {
+      const stream = ytdl(video.url, { filter: "audioonly" });
+      connection.play(stream, { seek: 0, volume: 1 }).on("finish", () => {
+        voiceChannel.leave();
+      });
+
+      await message.reply(`:notes: Now Playing ***${video.title}***`);
+    } else {
+      message.channel.send("No video results found");
+    }
+  },
+};
